d75523f8e75f6578e119705948bef8f7b4f33348
Julian Vassev <jvassev@gmail.com>
2016-09-12 23:52:15 +0300

    Distribute vagrant file

---
+++ _defaults/vagrant/config.rb
@@ -0,0 +1,4 @@
+$num_instances = 4
+$instance_name_prefix = "core"
+$update_channel = "beta"
+$image_version = "current"
--- start.sh
+++ start.sh
@@ -32,8 +32,6 @@ if [ "$DEVMODE" == "" ]; then
     mkdir /ansinetes/vagrant &> /dev/null
     pushd /ansinetes/vagrant > /dev/null
 
-    curl -Ls https://raw.githubusercontent.com/coreos/coreos-vagrant/master/Vagrantfile -O
-    sed -i 's/172\.17\.8/172.33.8/g' Vagrantfile
     cat << EOF > config.rb
 \$num_instances = 4
 \$instance_name_prefix = "core"
@@ -48,7 +46,9 @@ ssh_authorized_keys:
   - "$(cat /ansinetes/security/ansible-ssh-key.pub)"
   - "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
 EOF
+
   popd > /dev/null
+
   cat << EOF > .gitignore
 tmp/
 EOF
