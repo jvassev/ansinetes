044c5b8e68f34fd68db65eb39ffe10f42b8068a3
Julian Vassev <jvassev@gmail.com>
2016-09-20 23:17:12 +0300

    Force bash interpreter

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ if [ "$USER" != "ansinetes" ]; then
   if [ "$RUN_FILE" == "" ]; then
     exec su ansinetes -l
   else
-    exec su ansinetes $RUN_FILE
+    exec su ansinetes bash $RUN_FILE
   fi
 fi
 
