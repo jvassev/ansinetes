ebbd83d7e9100253182fa1dd44611d067b8964b0
Julian Vassev <jvassev@gmail.com>
2016-11-18 01:33:05 +0200

    Upgrade to k8s 1.4.5

--- _defaults/ansible/books/extract-k8s.sh
+++ _defaults/ansible/books/extract-k8s.sh
@@ -3,15 +3,14 @@
 TGZ=$1
 
 readonly K8S_DIR=k8s-bin
+mkdir ${K8S_DIR} &> /dev/null
 
-if [ -f kubernetes/server/bin/kube-apiserver ]; then
+if [ -f ${K8S_DIR}/kube-apiserver ]; then
 	exit 0
 fi
 
-mkdir ${K8S_DIR} &> /dev/null || (rm -frv ${K8S_DIR} && mkdir ${K8S_DIR})
-
-tar vxzf $TGZ kubernetes/server/kubernetes-server-linux-amd64.tar.gz --strip-components=2
-
-tar vxzf kubernetes-server-linux-amd64.tar.gz  kubernetes/server/bin
+tar vxzf $TGZ
 
 mv -v kubernetes/server/bin/{kube-scheduler,kube-proxy,kubelet,kubectl,kube-controller-manager,kube-apiserver,hyperkube} ${K8S_DIR}
+
+rm kubernetes -fr
